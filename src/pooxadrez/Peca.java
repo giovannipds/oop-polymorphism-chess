a98a29dd17ba5400caa6532cf70644c633668c9d
Giovanni Pires da Silva <giovanni.silva@avenuecode.com>
2023-04-20 20:07:10 -0300

    create an abstract piece class

---
+++ src/pooxadrez/Peca.java
@@ -0,0 +1,13 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package pooxadrez;
+
+/**
+ *
+ * @author giovannipds
+ */
+public abstract class Peca {
+    public abstract void mover();
+}
